068a2abf7c662e41b50627cf0ecaeb4a64fb6876
sugandha15 <33855087+sugandha15@users.noreply.github.com>
2020-05-05 16:06:29 +0530

    change database
    zero to two

--- src/test/java/com/example/devopscasestudy/DevopscasestudyApplicationTests.java
+++ src/test/java/com/example/devopscasestudy/DevopscasestudyApplicationTests.java
@@ -38,7 +38,8 @@ public void testFindAll()
 // if tables contains records 
 	
 List<Product> products = new ArrayList<>();
-//products.add(new Product());
+products.add(new Product());
+products.add(new Product());	
 
 when(productRepository.findAll()).thenReturn(products);
 
@@ -46,7 +47,7 @@ when(productRepository.findAll()).thenReturn(products);
 List<Product> result = productapi.findAll().getBody();
 
 // then
-assertThat(result.size()).isEqualTo(0);
+assertThat(result.size()).isEqualTo(2);
 
 // assertThat(result.get(0).getFirstName())
 // .isEqualTo(employee1.getFirstName());
