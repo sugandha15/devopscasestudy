7cd8b0e5421f271851651b836c33b79a0b7aa370
Sugandha15 <sugandha203@gmail.com>
2020-05-11 14:07:02 +0530

    clean smell changes

--- src/main/java/com/example/devopscasestudy/api/ProductAPI.java
+++ src/main/java/com/example/devopscasestudy/api/ProductAPI.java
@@ -2,7 +2,7 @@ package com.example.devopscasestudy.api;
 
 import java.util.List;
 
-import javax.validation.Valid;
+
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
